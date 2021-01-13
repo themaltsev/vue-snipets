3f7938e3ee89272e6bcfd11257c2bdbdb8ad04c6
Aleksandr Maltsev <47543995+themaltsev@users.noreply.github.com>
2021-01-13 14:58:12 +0300

    Create mapGetters.js

---
+++ mapGetters.js
@@ -0,0 +1,12 @@
+  
+  import {mapGetters} from 'vuex'
+  
+  //аналог
+  // computed:{
+  //   // allItems(){
+  //   //   return this.$store.getters.allItems
+  //   // }
+   
+  // },
+
+  computed: mapGetters(['allItems']),
